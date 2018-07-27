419196948d5e4fd9a7ccdc57b759b8c7612445e0
ShonP <pazshon1@gmail.com>
2018-07-27 18:06:57 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,33 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { Component } from "@angular/core";
+import { Platform } from "ionic-angular";
+import { StatusBar } from "@ionic-native/status-bar";
+import { SplashScreen } from "@ionic-native/splash-screen";
 
-import { HomePage } from '../pages/home/home';
+import { HomePage } from "../pages/home/home";
+import { Deeplinks } from "@ionic-native/deeplinks";
 @Component({
-  templateUrl: 'app.html'
+  templateUrl: "app.html"
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage: any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    private deeplinks: Deeplinks
+  ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
-  }
+    this.deeplinks.route({}).subscribe(
+      match => {
+        alert(JSON.stringify(match));
+      },
+      noMatch => alert(JSON.stringify(noMatch))
+    );
+  } //url:https://codepen.io/anon/pen/zLEjep
 }
-
